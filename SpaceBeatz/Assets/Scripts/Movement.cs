4c6d4c37174457fc81d2a644111067f9df8683d7
Pedro Esli <pedroesli@users.github.com>
2019-01-29 17:55:35 -0200

    Rocket Animation

--- SpaceBeatz/Assets/Scripts/Movement.cs
+++ SpaceBeatz/Assets/Scripts/Movement.cs
@@ -8,23 +8,41 @@ public class Movement : MonoBehaviour
     public Transform rightPoint;
     public Transform leftPoint;
     private Vector2 target;
+    private Animator anim;
 
     void Start()
     {
-        target = transform.position;
+        target = rightPoint.position;
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if ((Vector2)transform.position != target)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            if((Vector2)transform.position == target)
+            {
+                anim.SetBool("Rocket_rotate_right", false);
+                anim.SetBool("Rocket_rotate_left", false);
+            }
+        }
     }
 
     public void MoveRight()
     {
-        target = rightPoint.position;
+        if(transform.position != rightPoint.position)
+        {
+            target = rightPoint.position;
+            anim.SetBool("Rocket_rotate_right", true);
+        }
     }
     public void MoveLeft()
     {
-        target = leftPoint.position;
+        if (transform.position != leftPoint.position)
+        {
+            target = leftPoint.position;
+            anim.SetBool("Rocket_rotate_left", true);
+        } 
     }
 }

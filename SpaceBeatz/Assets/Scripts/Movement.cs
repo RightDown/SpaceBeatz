0780e8615e20b2a0dbcb3c84711f05a88f9de605
Pedro Esli <pedroesli@users.github.com>
2019-01-29 17:17:04 -0200

    First push
    game base

---
+++ SpaceBeatz/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public Transform rightPoint;
+    public Transform leftPoint;
+    private Vector2 target;
+
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    public void MoveRight()
+    {
+        target = rightPoint.position;
+    }
+    public void MoveLeft()
+    {
+        target = leftPoint.position;
+    }
+}
